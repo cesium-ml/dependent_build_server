aa70664a60645dd73c7df3529c9b45fd4f9ca3b7
Stefan van der Walt <stefanv@berkeley.edu>
2016-10-13 12:56:51 -0700

    Update for PythonAnywhere

--- dependent_build_server.py
+++ dependent_build_server.py
@@ -1,20 +1,10 @@
-import tornado.ioloop
 import tornado.web
-
+import tornado.wsgi
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello from Tornado")
 
-
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-
-
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(80
-    tornado.ioloop.IOLoop.current().start()
-
+application = tornado.wsgi.WSGIApplication([
+    (r"/", MainHandler),
+])
